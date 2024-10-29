d2b88aebc120b012cde583022c1bc04a08052ab8
Harry Zheng <zheng.harry@oakwoodschools.org>
2024-10-29 16:31:50 +0000

    3d array

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ endingproducts = targetproducts
 reaction1 = [["A"],["B","B"],57.9]
 reaction2 = [["C","C","D"],["B","B"],-113.1]
 
-reactions = [[["A"],["B","B"],57.9],[["C","C","D"],["B","B"],-113.1]]
+reactions = [
+[["A"],["B","B"],57.9],
+[["C","C","D"],["B","B"],-113.1]
+]
 
 totalenthalpy =0
 
@@ -27,110 +30,132 @@ for l in range(50):
     x=x[0]
   except:
     break
-  reaction1used = False
-  reaction2used = False
-  try:
-    for j in range(len(reaction1[0])):
-
-      if reaction1[0][j] == endingreactants[i]:
-        k=0
-        totalenthalpy += reaction1[2]
-        reaction1used = True
-        for k in range(len(reaction1[0])):
-          try:
-            endingreactants.remove(reaction1[0][k])
-
-          except:
-            endingproducts.append(reaction1[0][k])
-        k=0
-        for k in range(len(reaction1[1])):
-          try:
-            endingproducts.remove(reaction1[1][k])
-          except:
-            endingreactants.append(reaction1[1][k])
-  except:
-    pass
-  print()
-  print(endingreactants)
-  print(endingproducts)
-  print()
-  j=0
-  try:
-    for j in range(len(reaction2[0])):
-      if reaction2[0][j] == endingreactants[i]:
-        totalenthalpy += reaction2[2]
-        reaction2used = True
-        k=0
-        for k in range(len(reaction2[0])):
-          try:
-            endingreactants.remove(reaction2[0][k])
-          except:
-            endingproducts.append(reaction2[0][k])
-        k=0
-        for k in range(len(reaction2[1])):
-          try:
-            endingproducts.remove(reaction2[1][k])
-          except:
-            endingreactants.append(reaction2[1][k])
-  except:
-    pass
-  print()
-  print(endingreactants)
-  print(endingproducts)
-  print()
-  if reaction1used == False:
+  reactionsused = [False,False]
+  for reaction in range(len(reactions)):
     try:
-      for j in range(len(reaction1[1])):
-        # print(endingreactants[i])
-        # print()
-        # print(reaction1[1][j])
-        # print("------")
-        if reaction1[1][j] == endingreactants[i]:
-          k=0
-          totalenthalpy -= reaction1[2]
-          for k in range(len(reaction1[0])):
+      for j in range(len(reactions[reaction][0])):
 
+        if reactions[reaction][0][j] == endingreactants[i]:
+          k=0
+          totalenthalpy += reactions[reaction][2]
+          reaction1used = True
+          for k in range(len(reactions[reaction][0])):
             try:
-              endingproducts.remove(reaction1[0][k])
+              endingreactants.remove(reactions[reaction][0][k])
 
             except:
-              endingreactants.append(reaction1[0][k])
-
+              endingproducts.append(reactions[reaction][0][k])
           k=0
-          for k in range(len(reaction1[1])):
+          for k in range(len(reactions[reaction][1])):
             try:
-              endingreactants.remove(reaction1[1][k])
+              endingproducts.remove(reactions[reaction][1][k])
             except:
-              endingproducts.append(reaction1[1][k])
+              endingreactants.append(reactions[reaction][1][k])
     except:
       pass
+    if reactionsused == False:
+      try:
+        for j in range(len(reactions[reaction][1])):
+          if reactions[reaction][1][j] == endingreactants[i]:
+            k=0
+            totalenthalpy -= reactions[reaction][2]
+            for k in range(len(reactions[reaction][0])):
+
+              try:
+                endingproducts.remove(reactions[reaction][0][k])
+
+              except:
+                endingreactants.append(reactions[reaction][0][k])
+
+            k=0
+            for k in range(len(reactions[reaction][1])):
+              try:
+                endingreactants.remove(reactions[reaction][1][k])
+              except:
+                endingproducts.append(reactions[reaction][1][k])
+      except:
+        pass
   print()
   print(endingreactants)
   print(endingproducts)
   print()
-  if reaction2used == False:
-    try:
-      for j in range(len(reaction2[1])):
-        if reaction2[1][j] == endingreactants[i]:
-          totalenthalpy -= reaction2[2]
-          k=0
-          for k in range(len(reaction2[0])):
-            try:
-              endingproducts.remove(reaction2[0][k])
-            except:
-              endingreactants.append(reaction2[0][k])
-          k=0
-          for k in range(len(reaction2[1])):
-            try:
-              endingreactants.remove(reaction2[1][k])
-            except:
-              endingproducts.append(reaction2[1][k])
-    except:
-      pass
-  print()
-  print(endingreactants)
-  print(endingproducts)
-  print("-----------------------------------------------------------------")
+  # j=0
+  # try:
+  #   for j in range(len(reaction2[0])):
+  #     if reaction2[0][j] == endingreactants[i]:
+  #       totalenthalpy += reaction2[2]
+  #       reaction2used = True
+  #       k=0
+  #       for k in range(len(reaction2[0])):
+  #         try:
+  #           endingreactants.remove(reaction2[0][k])
+  #         except:
+  #           endingproducts.append(reaction2[0][k])
+  #       k=0
+  #       for k in range(len(reaction2[1])):
+  #         try:
+  #           endingproducts.remove(reaction2[1][k])
+  #         except:
+  #           endingreactants.append(reaction2[1][k])
+  # except:
+  #   pass
+  # print()
+  # print(endingreactants)
+  # print(endingproducts)
+  # print()
+  # if reaction1used == False:
+  #   try:
+  #     for j in range(len(reaction1[1])):
+  #       # print(endingreactants[i])
+  #       # print()
+  #       # print(reaction1[1][j])
+  #       # print("------")
+  #       if reaction1[1][j] == endingreactants[i]:
+  #         k=0
+  #         totalenthalpy -= reaction1[2]
+  #         for k in range(len(reaction1[0])):
+
+  #           try:
+  #             endingproducts.remove(reaction1[0][k])
+
+  #           except:
+  #             endingreactants.append(reaction1[0][k])
+
+  #         k=0
+  #         for k in range(len(reaction1[1])):
+  #           try:
+  #             endingreactants.remove(reaction1[1][k])
+  #           except:
+  #             endingproducts.append(reaction1[1][k])
+  #   except:
+  #     pass
+  # print()
+  # print(endingreactants)
+  # print(endingproducts)
+  # print()
+  # if reaction2used == False:
+  #   try:
+  #     for j in range(len(reaction2[1])):
+  #       if reaction2[1][j] == endingreactants[i]:
+  #         totalenthalpy -= reaction2[2]
+  #         k=0
+  #         for k in range(len(reaction2[0])):
+  #           try:
+  #             endingproducts.remove(reaction2[0][k])
+  #           except:
+  #             endingreactants.append(reaction2[0][k])
+  #         k=0
+  #         for k in range(len(reaction2[1])):
+  #           try:
+  #             endingreactants.remove(reaction2[1][k])
+  #           except:
+  #             endingproducts.append(reaction2[1][k])
+  #   except:
+  #     pass
+  # print()
+  # print(endingreactants)
+  # print(endingproducts)
+  # print("-----------------------------------------------------------------")
   i+=1
 
 print(endingreactants)
